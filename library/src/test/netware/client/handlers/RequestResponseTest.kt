a31c474ced50fb43bbe2d2de07eea96c926f363b
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-13 12:08:55 +0530

    Create test for unformatted log

--- library/src/test/netware/client/handlers/RequestResponseTest.kt
+++ library/src/test/netware/client/handlers/RequestResponseTest.kt
@@ -1,4 +1,23 @@
 package netware.client.handlers
 
+import netware.client.holders.RequestResponse
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class RequestResponseTest {
+
+    @Test
+    fun testUnformattedLog() {
+
+        val requestResponse = RequestResponse(
+            statusCode = 200,
+            status = "OK",
+            response = "Hey"
+        )
+
+        assertEquals("""
+            Status code: 200, Status: OK.
+            Response: Hey
+        """.trimIndent(), requestResponse.getLog())
+    }
 }
