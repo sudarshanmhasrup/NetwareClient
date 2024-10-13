140fbfd8556cda01f93e4e41aa45d26b8dfa43cd
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-13 18:51:49 +0530

    Add secondary constructors for RequestClient

--- library/src/main/netware/client/RequestClient.kt
+++ library/src/main/netware/client/RequestClient.kt
@@ -1,5 +1,6 @@
 package netware.client
 
+import com.google.gson.JsonObject
 import netware.client.callbacks.ClientCallback
 import netware.client.holders.RequestError
 import netware.client.holders.RequestResponse
@@ -11,9 +12,39 @@ class RequestClient(
 
     // Network request properties
     private val networkRequestUrl = url
-    private val networkRequestMethod = "GET"
+    private var networkRequestMethod = "GET"
     private val networkRequestHeaders = mutableMapOf<String, String>()
-    private val networkRequestBody: String? = null
+    private var networkRequestBody: JsonObject? = null
+
+    // Secondary constructor 1
+    constructor(url: String, method: String): this(url) {
+        networkRequestMethod = method
+    }
+
+    // Secondary constructor 2
+    constructor(url: String, method: String, headers: Map<String, String>): this(url) {
+        networkRequestMethod = method
+        addHeaders(headers)
+    }
+
+    // Secondary constructor 3
+    constructor(url: String, method: String, headers: Map<String, String>, body: JsonObject): this(url) {
+        networkRequestMethod = method
+        addHeaders(headers)
+        networkRequestBody = body
+    }
+
+    // Secondary constructor 4
+    constructor(url: String, headers: Map<String, String>): this(url) {
+        networkRequestMethod = "GET"
+        addHeaders(headers)
+    }
+
+    // Secondary constructor 4
+    constructor(url: String, method: String, body: JsonObject): this(url) {
+        networkRequestMethod = method
+        networkRequestBody = body
+    }
 
     // Network request response
     var response = RequestResponse()
@@ -29,4 +60,15 @@ class RequestClient(
     fun build(): RequestClient {
         return this
     }
+
+    private fun addHeaders(headers: Map<String, String>) {
+        for ((key, value) in headers) {
+            networkRequestHeaders[key] = value
+        }
+    }
+
+    internal fun getNetworkRequestUrl() = networkRequestUrl
+    internal fun getNetworkRequestMethod() = networkRequestMethod
+    internal fun getNetworkRequestHeaders() = networkRequestHeaders
+    internal fun getNetworkRequestBody() = networkRequestBody
 }
