e2fed31ad2c05b9de1aaa0f07998891d62bd6989
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-13 11:38:25 +0530

    Create RequestClient class

---
+++ library/src/main/netware/client/RequestClient.kt
@@ -0,0 +1,14 @@
+package netware.client
+
+class RequestClient(
+    private val url: String
+) {
+
+    // Network request properties
+    private val networkRequestUrl = url
+    private val networkRequestMethod = "GET"
+    private val networkRequestHeaders = mutableMapOf<String, String>()
+    private val networkRequestBody: String? = null
+
+    // Network request response
+}
