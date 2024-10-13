4f28f36f94e0caae70e06f6d23a36c9699261716
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-13 11:31:19 +0530

    Define source and test directory

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,9 +3,19 @@ plugins {
 }
 
 repositories {
-
+    mavenCentral()
 }
 
 dependencies {
 
+}
+
+// Source and test directory
+sourceSets {
+    main {
+        kotlin.srcDirs("src/main")
+    }
+    test  {
+        kotlin.srcDirs("src/test")
+    }
 }
