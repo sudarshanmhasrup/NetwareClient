5e95c6386cd040bee07e8d7d0f8e487039c3af09
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-14 19:06:14 +0530

    Add headers to custom greetings controller test request

--- api/src/test/com/github/sudarshanmhasrup/api/controllers/customGreetings/CustomGreetingsControllerTest.kt
+++ api/src/test/com/github/sudarshanmhasrup/api/controllers/customGreetings/CustomGreetingsControllerTest.kt
@@ -1,6 +1,8 @@
 package com.github.sudarshanmhasrup.api.controllers.customGreetings
 
 import netware.client.RequestClient
+import netware.client.extensions.RequestHeaders
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class CustomGreetingsControllerTest {
@@ -8,16 +10,19 @@ class CustomGreetingsControllerTest {
     @Test
     fun testCustomGreetingsController() {
 
+        val requestHeaders = RequestHeaders()
+        requestHeaders.add("Content-Type", "text/plain")
+
         val requestClient = RequestClient(
             url = "http://localhost:8000/v1/display-custom-message",
             method = "POST",
-            body = """
-                {"message":"Hello, I'm Sudarshan!"
-            """.trimIndent()
+            headers = requestHeaders.getHeaders(),
+            body = "Hello World"
         ).build()
 
-        println(requestClient.response().getLog(
-            isFormatted = true
-        ))
+        assertEquals("""
+            Status code: 200, Status: No status found.
+            Response: {"statusCode":200,"message":"Hello World"}
+        """.trimIndent(), requestClient.response().getLog())
     }
 }
