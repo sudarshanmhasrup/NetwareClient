704a0f419e897c4fc5e2527d4c086e020f78f2d6
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-14 14:32:10 +0530

    Add Kotlin serialization plugin to version catalog

--- api/src/test/com/github/sudarshanmhasrup/api/controllers/customGreetings/CustomGreetingsResponse.kt
+++ api/src/test/com/github/sudarshanmhasrup/api/controllers/customGreetings/CustomGreetingsResponse.kt
@@ -1,5 +1,7 @@
 package com.github.sudarshanmhasrup.api.controllers.customGreetings
 
+import kotlinx.serialization.Serializable
+
 @Serializable
 data class CustomGreetingsResponse(
     val statusCode: Int,
