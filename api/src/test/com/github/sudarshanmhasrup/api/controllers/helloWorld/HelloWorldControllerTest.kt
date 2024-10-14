0f961ab493e1a61a00ca3d6cf0a995a169404a67
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-14 13:41:11 +0530

    Create test for testing /v1/hello-world API endpoint

--- api/src/test/com/github/sudarshanmhasrup/api/controllers/helloWorld/HelloWorldControllerTest.kt
+++ api/src/test/com/github/sudarshanmhasrup/api/controllers/helloWorld/HelloWorldControllerTest.kt
@@ -1,11 +1,18 @@
 package com.github.sudarshanmhasrup.api.controllers.helloWorld
 
+import netware.client.RequestClient
 import org.junit.jupiter.api.Test
 
 class HelloWorldControllerTest {
 
     @Test
     fun testHelloWorldEndpoint() {
+
+        val requestClient = RequestClient(
+            url = "http://localhost:8080/v1/hello-world",
+            method = "GET"
+        ).build()
+
 
     }
 }
