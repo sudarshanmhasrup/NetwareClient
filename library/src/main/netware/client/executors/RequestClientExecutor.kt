0044eed282004238d5f44bd951d869e75e5cb07b
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-14 10:25:09 +0530

    Create test to check log for invalid request protocal error

--- library/src/main/netware/client/executors/RequestClientExecutor.kt
+++ library/src/main/netware/client/executors/RequestClientExecutor.kt
@@ -1,14 +1,37 @@
 package netware.client.executors
 
+import netware.client.holders.HttpResponseContainer
+import netware.client.holders.RequestError
+
 // Class to execute network requests for RequestClient
-class RequestClientExecutor(
+internal class RequestClientExecutor(
     private val networkRequestUrl: String,
     private val networkRequestMethod: String,
     private val networkRequestHeaders: Map<String, String>? = null,
     private val networkRequestBody: String? = null
 ) {
 
-    internal fun validateNetworkRequest() {
+    internal fun validateNetworkRequest(): HttpResponseContainer {
+        return when {
+            networkRequestUrl.startsWith("http://") -> {
+                executeRequestUrl(isHttp = false)
+            }
+            networkRequestUrl.startsWith("https://") -> {
+                executeRequestUrl(isHttp = true)
+            }
+            else ->
+                HttpResponseContainer(
+                    isSuccess = false,
+                    requestError = RequestError(
+                        statusCode = 400,
+                        status = "Bad request",
+                        message = "The request protocol is invalid."
+                    )
+                )
+        }
+    }
 
+    internal fun executeRequestUrl(isHttp: Boolean): HttpResponseContainer {
+        return HttpResponseContainer()
     }
 }
---
+++ library/src/test/netware/client/executors/RequestClientExecutorTest.kt
@@ -0,0 +1,21 @@
+package netware.client.executors
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RequestClientExecutorTest {
+
+    @Test
+    fun testInvalidRequestProtocolLog() {
+
+        val requestClientExecutor = RequestClientExecutor(
+            networkRequestUrl = "postgresql://localhost:3000",
+            networkRequestMethod = "GET"
+        )
+
+        assertEquals("""
+            Status code: 400, Status: Bad request.
+            Message: The request protocol is invalid.
+        """.trimIndent(), requestClientExecutor.validateNetworkRequest().getRequestError().getLog())
+    }
+}
