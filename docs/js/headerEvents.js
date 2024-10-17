0247c6ee1232ac64d47c4c72ae70819102cebfae
Sudarshan Mhasrup <sudarshanmhasrup@outlook.com>
2024-10-17 16:25:52 +0530

    Remove docs folder

--- docs/js/headerEvents.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const githubLinkButton = document.getElementById("github-link-logo-button");
-
-    githubLinkButton.addEventListener("click", () => {
-        window.open("https://github.com/sudarshanmhasrup/netwareclient");
-    });
-});
